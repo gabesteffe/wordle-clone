afe0de1a516c9de5b2c43d88d22920f4aa9bbed9
Gabe <gabe@Kasss-MacBook-Air.local>
2022-03-21 12:43:46 -0600

    restructured js, added win and lose conditions, added functionality to blackout keys that are not in solution, started working on CSS animation for win condition

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/*
+TODO:
+    - update CSS to make UI look cleaner
+        - adjust size, spacing and borders of letter tiles (possibly round off the corners?)
+        - adjust the placement and padding of the button and text field
+        - consider making the letters appear within the current row
+    - add functionality so that users can append / remove letters from their current guess with the keyboard, as well as submit a guess
+    - play an animation when the player wins
+    - generate the secret word randomly
+    - give the option to play again once the game is over
+    - keep track of statistics across games
+    - implement customizable word lengths
+
+*/
+
 let secretWord = "snore"
 
 let submitBtn = document.getElementById("submitBtn")
@@ -6,23 +21,49 @@ let container = document.getElementsByClassName("container")[0]
 let keyboardContainer = document.getElementsByClassName("keyboardContainer")[0]
 let divs = []
 let guessCounter = 0
+const guessLimit = 5
 
 function main(){
     populateDivs();
     populateKeyboard();
-    submitBtn.onclick = () => {
-        guess = guessText.value
-        if (validateGuess(guess)){
-            let results = guessWord(guess)
-            updateRow(guess, results)
-            guessCounter++
-        } else {
-            console.log(`That guess was not valid, please submit a guess with ${secretWord.length} characters`)
-        }
-    }
+    submitBtn.onclick = attemptGuess
 }
 
-function validateGuess(guess){
+function attemptGuess(){
+    /*
+    - validate the guess
+    - evaluate the guess
+        - determine which letters in the guess are in the solution and if the positions are yellow or green
+        - handle the win condition
+        - handle the loss condition
+    - update the row to reflect guess evaluation
+    - update the keybord to reflect guess evaluation
+    - increment guess counter
+    */
+    let guess = guessText.value
+    if (!isGuessValid(guess)){
+        console.log(`That guess was not valid, please submit a guess with ${secretWord.length} characters`)
+    }
+
+    let results = evaluateGuess(guess)
+
+    let lettersNotInWord = []
+    for (let i = 0 ; i < guess.length ; i++){
+        if (results[i] === "-"){
+            lettersNotInWord.push(guess[i])
+        }
+    }
+
+    updateRow(guess, results)
+    updateKeyboard(lettersNotInWord)
+
+    //check for win condition and for loss condition
+    evaluateGameState(results)
+    guessCounter++
+
+}
+
+function isGuessValid(guess){
     return (guess.length === secretWord.length ? true : false)
 }
 
@@ -53,29 +94,27 @@ function populateKeyboard(){
     }
 }
 
-function guessWord(guess){
+/*
+    returns an array made up of three possible characters, "G" for a green letter, "Y" for a yellow letter and "-" for a letter
+    that doesn't appear in the guess
+*/
+function evaluateGuess(guess){
     let results = []
     for (var i = 0 ; i < guess.length; i++){
-        results.push(letterAppearsInSecretWord(guess, i))
+        results.push(evaluateLetter(guess, i))
     }
+    
     return results
 }
 
-function letterAppearsInSecretWord(word, letterIndex){
+function evaluateLetter(word, letterIndex){
     letter = word[letterIndex]
-    let letterFound = false
     let greenFlag = false
     let yellowFlag = false
     //iterate through all letters in the secret word to see if the letter from the guess is there
     for (var i = 0; i < secretWord.length; i++){
         if (secretWord[i] === letter){
-            if (letterIndex === i){
-                greenFlag = true
-                console.log("the " + letter + " character at position " + (i+1) + " is correct")
-            } else {
-                yellowFlag = true
-                console.log("the " + letter + " character is in the secret word, but not at position " + (i+1))
-            }
+            letterIndex === i ? greenFlag = true : yellowFlag = true;
         } 
     }
     if (greenFlag){
@@ -85,20 +124,51 @@ function letterAppearsInSecretWord(word, letterIndex){
     }
     else{
         return "-"
-        console.log("the " + letter + " character is in not the secret word")
+    }
+}
+
+function evaluateGameState(guessResults){
+    let winningGuess = true;
+    for (char of guessResults){
+        if (char !== "G"){
+            winningGuess = false
+            break
+        }
+    }
+    if (winningGuess){
+        revokeSubmitBtnFunctionality()
+        console.log("congrats, you won!")
+    } else {
+        if (guessCounter === guessLimit){
+            console.log("Oh no, you lost! better luck next time!")
+            revokeSubmitBtnFunctionality()
+        }
+    }
+}
+
+function updateKeyboard(letters){
+    for (letter of letters){
+        //use the ascii code from the letter to determine its index
+        let index = letter.toLowerCase().charCodeAt(0) - 97
+        //black out that div
+        keyboardContainer.children[index].classList.add("outsideOfSolution")
     }
 }
 
 function updateRow(guessWord, results){
     for (i = 0; i < guessWord.length ; i++){
         let letterTile = divs[guessCounter].children[i]
-        letterTile.innerText = guess[i]
+        letterTile.innerText = guessWord[i]
         if (results[i] === 'G'){
             letterTile.classList.add('greenLetterBox')
         } else if (results[i] === 'Y'){
             letterTile.classList.add('yellowLetterBox')
         }
     }
+}
+
+function revokeSubmitBtnFunctionality(){
+    submitBtn.onclick = () => {}
 }
 
 main()
